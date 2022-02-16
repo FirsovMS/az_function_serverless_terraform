bc7da2cb907c6233375906cc08c8cadcac712e5c
Mikhail_Firsov <firsovms1993@gmail.com>
2022-02-17 00:07:16 +0500

    update main.tf and add zip-storage suppoer

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "storage_accout" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = "${var.project}${var.environment}storage"
   location                 = var.location
   resource_group_name      = azurerm_resource_group.resource_group.name
@@ -43,18 +43,57 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   }
 }
 
+data "archive_file" "app_zip" {
+  type        = "zip"
+  source_dir  = "../dist"
+  output_path = "./app.zip"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "function-releases"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+data "azurerm_storage_account_blob_container_sas" "storage_account_blob_container_sas" {
+  connection_string = azurerm_storage_account.storage_account.primary_connection_string
+  container_name    = azurerm_storage_container.storage_container.name
+
+  start  = "2022-01-01T00:00:00Z"
+  expiry = "2023-01-01T00:00:00Z"
+
+  permissions {
+    read   = true
+    add    = false
+    create = false
+    write  = false
+    delete = false
+    list   = false
+  }
+}
+
+resource "azurerm_storage_blob" "storage_blob" {
+  name                   = "${filesha256(data.archive_file.app_zip.output_path)}.zip"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Block"
+  source                 = data.archive_file.app_zip.output_path
+}
+
 resource "azurerm_function_app" "function_app" {
   name                = "${var.project}-${var.environment}-function-app"
   location            = var.location
   resource_group_name = azurerm_resource_group.resource_group.name
   app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE"       = "",
+    # use zip-package strategy
+    "WEBSITE_RUN_FROM_PACKAGE"       = "https://${azurerm_storage_account.storage_account.name}.blob.core.windows.net/${azurerm_storage_container.storage_container.name}/${azurerm_storage_blob.storage_blob.name}${data.azurerm_storage_account_blob_container_sas.storage_account_blob_container_sas.sas}",
     "FUNCTIONS_WORKER_RUNTIME"       = "node",
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
+    "AzureWebJobsDisableHomepage"    = "true",
   }
-  storage_account_name       = azurerm_storage_account.storage_accout.name
-  storage_account_access_key = azurerm_storage_account.storage_accout.primary_access_key
+  storage_account_name       = azurerm_storage_account.storage_account.name
+  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
   os_type                    = "linux"
   version                    = "~3"
 
@@ -69,4 +108,3 @@ resource "azurerm_function_app" "function_app" {
     ]
   }
 }
-
